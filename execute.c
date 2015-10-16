749df16faba5b39b0117bd5ad137dcab46e7e642
sK0pe <pradyumn.vij@gmail.com>
2015-10-16 15:19:36 +0800

    Step 1 Single Commands Completed Can execute simple command now.

--- execute.c
+++ execute.c
@@ -1,4 +1,6 @@
 #include "mysh.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /*
    CITS2002 Project 2 2015
@@ -15,19 +17,38 @@
 
 int execute_cmdtree(CMDTREE *t)
 {
-	if(t->type == N_COMMAND){
-		printf("N command has been found");
-		if(fork() == 0){	//	child process successfully formed
-			char *newargs[] = {"ls", "-l", NULL};
-			execv("/bin/ls", newargs);
-		}
-		else{
+	switch(t->type){
+		case N_COMMAND:{
+			int status;  //  used by wait, to check on child process
+			pid_t programID;
 			
+			programID = fork();
+
+			if(programID < 0){
+				//fprintf(stderr, "Forking %s failed\n", *t->argv);
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+			//  If fork succeeds
+			if(programID == 0){	//  child process successfully initiated
+				if(execvp(*t->argv, t->argv) < 0){	//  if execute fails
+					//fprintf(stderr, "Execution of %s failed.\n", *t->argv);
+					perror("mysh");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else{	// parent process (mysh) waiting for child to end
+				while(wait(&status) != programID);
+			}
+			break;
 		}
+
+		default :
+			fprintf(stderr,"%s: invalid NODETYPE in print_cmdtree0()\n",argv0);
+			exit(1);
+			break;
 	}
-	else{
-		fprintf(stderr,"%s: invalid NODETYPE in execute_cmdtree()\n",argv0);
-	}
+	
 
 	int  exitstatus;
 	if(t == NULL) {			// hmmmm, a that's problem
